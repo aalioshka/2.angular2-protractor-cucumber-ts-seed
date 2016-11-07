9b914836056af8068265c5ba5461566b312ad9ae
Alioshka, Aliaksei <Aliaksei_Alioshka@epam.com>
2016-11-07 14:04:14 +0300

    update some code

--- e2e/step_definitions/support/hooks.ts
+++ e2e/step_definitions/support/hooks.ts
@@ -1,4 +1,13 @@
-export  = function() {
+import {browser} from "protractor";
+export  = function () {
     const DEFAULT_STEP_TIMEOUT = 360 * 1000;
     this.setDefaultTimeout(DEFAULT_STEP_TIMEOUT);
+
+    this.After((scenario, done) => {
+            browser.takeScreenshot().then(base64png => {
+                let decodedImage = new Buffer(base64png, 'base64');
+                scenario.attach(decodedImage, 'image/png');
+            })
+            .then(done);
+    });
 }
